8b157bd1c11fe700ac313c803011cfe17d62bc1e
Silvain Toromanoff <silvain.toromanoff@sayduck.com>
2021-03-03 14:40:37 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  match "*path" => redirect("https://www.silvain.space/%{path}"), :constraints => { :protocol => "http://" }
-  match "*path" => redirect("https://www.silvain.space/%{path}"), :constraints => { :subdomain => "" }
+  match "*path" => redirect("https://www.silvain.space/%{path}"), :constraints => { :protocol => "http://" }, via: [:get, :post]
+  match "*path" => redirect("https://www.silvain.space/%{path}"), :constraints => { :subdomain => "" }, via: [:get, :post]
 end
